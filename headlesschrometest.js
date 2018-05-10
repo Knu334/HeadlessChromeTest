40fd97db94c34465b4f48eb8f44ec0ed1968dec3
Knu334 <who.is.fuu@gmail.com>
2018-05-11 00:09:07 +0900

    m3u8取得機能追加

--- headlesschrometest.js
+++ headlesschrometest.js
@@ -1,11 +1,33 @@
 const puppeteer = require('puppeteer');
 
-(async() => {
-	const browser = await puppeteer.launch();
+async function sleep(delay) {
+	return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+puppeteer.launch().then(async browser => {
 	const page = await browser.newPage();
+	const options = {
+		
+		'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
+		'viewport': {
+			'width': 1920,
+			'height': 1080
+		}
+	};
+	await page.emulate(options);
+	await page.setExtraHTTPHeaders({'Accept-Language': 'ja,en-US;q=0.9,en;q=0.8'});
+	await page.setRequestInterception(true);
+
+	page.on('request', interceptedRequest => {
+		if (interceptedRequest.url().endsWith('.m3u8')) {
+			console.log(interceptedRequest.url());
+		}
+		interceptedRequest.continue();
+	});
 
 	await page.goto('');
-	await page.screenshot({path: 'test.png', fillPage: true});
+	await sleep(1000);
+	//await page.screenshot({path: 'test.png', fullPage: true});
 
-	browser.close();
-})();
+	await browser.close();
+});
